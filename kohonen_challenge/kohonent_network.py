fb747a67320eb77cab6e7d4913fef427b99f5ac7
Peng Hao <peng.hao@elulagroup.com>
2020-10-19 14:51:56 +1100

    add code optimizaiton

--- kohonen_challenge/kohonent_network.py
+++ kohonen_challenge/kohonent_network.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Python implementation of Self-Organising Map (SOM).
+"""
 
 
 import numpy as np
+import numba
 
 
 class SOM(object):
@@ -40,13 +43,9 @@ class SOM(object):
         Args:
             data: given input data.
         """
-        self._map = np.random.random((self._height, self._width, data.shape[1]))
+        self._map = np.random.normal(size=(self._height, self._width, data.shape[1]))
         self._initialized = True
 
-    # TODO: Need to be optimized
-    # Two options:
-    # 1. vectorize computation with numpy
-    # 2. using Numba to speed up.
     def _calc_closet_node(self, input_vect: np.ndarray) -> np.ndarray:
         """Calculate the node that is closest to the input data point.
 
@@ -56,18 +55,10 @@ class SOM(object):
         Returns:
             index of the node (BMU) in SOM
         """
-        dist_mat = np.sqrt(np.sum((self._map - input_vect)**2, axis=2))
+        dist_mat = np.sqrt(np.sum((self._map - input_vect) ** 2, axis=2))
         bmu_idx = np.where(dist_mat == np.amin(dist_mat))
         return np.array(bmu_idx).reshape(-1)
 
-
-    @staticmethod
-    def _calculate_influence(distance, radius) -> float:
-        return np.exp(-distance / (2 * (radius ** 2)))
-
-    # TODO: Need to optimized.
-    # TODO: 1. vectorize computation with numpy
-    # TODO: 2. using Numba to speed up.
     def _fit_on_data(self, input_vect: np.ndarray, epoch: int) -> None:
         """For each iteration,
         adapt SOM weights towards a given data point.
@@ -79,18 +70,55 @@ class SOM(object):
         bmu_idx = self._calc_closet_node(input_vect)
         for x in range(self._height):
             for y in range(self._width):
-                w = self._map[x, y, :]
-                dist = np.sqrt(np.sum((np.array([x, y]) - bmu_idx) ** 2))
+                self._map[x, y, :] = self._update_node_weights(
+                    input_vect,
+                    node_weights=self._map[x, y, :],
+                    node_idx=np.array([x, y]),
+                    bmu_idx=bmu_idx,
+                    radius=self._sigmas[epoch],
+                    learning_rate=self._alphas[epoch],
+                )
+        print(
+            f"Epoch={epoch}, BMU_idx={[bmu_idx[0], bmu_idx[1]]}, radius="
+            f"{self._sigmas[epoch]}, learning_rate={self._alphas[epoch]}"
+        )
 
-                if dist <= self._sigmas[epoch]:
-                    # Calculate influence for neighbour node
-                    influence = self._calculate_influence(dist, self._sigmas[epoch])
+    @staticmethod
+    @numba.jit(nopython=True)
+    def _update_node_weights(
+        input_vect: np.ndarray,
+        node_weights: np.ndarray,
+        node_idx: np.ndarray,
+        bmu_idx: np.ndarray,
+        radius: float,
+        learning_rate: float,
+    ) -> np.ndarray:
+        """Update weights of the given neighbour node according to BMU.
 
-                    # Update weight matrix
-                    new_w = w + (self._alphas[epoch] * influence * (input_vect - w))
-                    self._map[x, y, :] = new_w
-        print(f"Epoch={epoch}, BMU_idx={[bmu_idx[0], bmu_idx[1]]}, radius="
-              f"{self._sigmas[epoch]}, learning_rate={self._alphas[epoch]}")
+        Args:
+            input_vect: input data point.
+            node_weights: weights vector for a node in the network.
+            node_idx: x and y coordinates for the given node.
+            bmu_idx: x and y coordinates for the BMU node.
+            radius: the radius for this iteration.
+            learning_rate: the learning rate for this iteration.
+
+        Returns:
+            Updated weights for the given neighbour node.
+        """
+        dist = np.sqrt(np.sum((node_idx - bmu_idx) ** 2))
+
+        if dist <= radius:
+            # Calculate influence for neighbour node
+            influence = np.exp(-dist / (2 * (radius ** 2)))
+
+            # Update weight matrix
+            new_weights = node_weights + (
+                learning_rate * influence * (input_vect - node_weights)
+            )
+            return new_weights
+        else:
+            return node_weights
 
     def _calc_decays_per_iteration(self) -> np.ndarray:
         """Calculate decay weight per iteration.
